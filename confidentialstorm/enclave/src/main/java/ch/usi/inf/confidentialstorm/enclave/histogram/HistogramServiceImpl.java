c0a4cc9420be8978c32088c9a429de2b2c8b78cf
Luca Di Bello <lucadibello@proton.me>
2025-11-10 17:12:05 +0000

    fix: using normal HashMap rather than ConcurrentHashMap

--- confidentialstorm/enclave/src/main/java/ch/usi/inf/confidentialstorm/enclave/histogram/HistogramServiceImpl.java
+++ confidentialstorm/enclave/src/main/java/ch/usi/inf/confidentialstorm/enclave/histogram/HistogramServiceImpl.java
@@ -1,26 +1,26 @@
 package ch.usi.inf.confidentialstorm.enclave.histogram;
 
 import ch.usi.inf.confidentialstorm.common.api.HistogramService;
-import ch.usi.inf.confidentialstorm.common.model.HistogramSnapshot;
-import ch.usi.inf.confidentialstorm.common.model.HistogramUpdate;
+import ch.usi.inf.confidentialstorm.common.model.HistogramSnapshotResponse;
+import ch.usi.inf.confidentialstorm.common.model.HistogramUpdateRequest;
 import com.google.auto.service.AutoService;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 @AutoService(HistogramService.class)
 public class HistogramServiceImpl implements HistogramService {
-    private final ConcurrentMap<String, Long> histogram = new ConcurrentHashMap<>();
+    private final Map<String, Long> histogram = new HashMap<>();
 
     @Override
-    public void update(HistogramUpdate update) {
+    public void update(HistogramUpdateRequest update) {
         histogram.put(update.word(), update.count());
     }
 
     @Override
-    public HistogramSnapshot snapshot() {
-        return new HistogramSnapshot(new HashMap<>(histogram));
+    public HistogramSnapshotResponse snapshot() {
+        // return a copy to avoid external modification
+        // NOTE: made immutable by the HistogramSnapshot constructor to avoid serialization issues
+        return new HistogramSnapshotResponse(histogram);
     }
 }
